2d27a5f34dee171fde6ad5fcae6ff106de6bbb8b
Nick Hengeveld <nickh@verticalresponse.com>
2010-12-01 13:47:18 -0800

    Added a substr algorithm to the mix, dried up the runner

--- jeebus.rb
+++ jeebus.rb
@@ -2,33 +2,44 @@ require 'rubygems'
 require 'benchmark'
 require 'ruby-prof'
 
-result = RubyProf.profile do
-  "messages.index.filter.label".split('.').reverse.inject([]){|a,s| a << [s,a.last].compact.join('.').to_sym}.reverse
+def with_inject(s)
+  segments = s.split('.').reverse
+  segments.inject([segments.shift.to_sym]){|a,s| a.unshift "#{s}.#{a.first}".to_sym}
 end
 
-RubyProf::GraphPrinter.new(result).print(STDOUT, 0)
-
-result = RubyProf.profile do
-  a = "messages.index.filter.label".split('.')
+def with_block(s)
+  a = s.split('.')
   1.upto(a.size).inject([]) {|s,t| s << a.slice(a.size-t,t).join('.').to_sym}.reverse
 end
 
-RubyProf::GraphPrinter.new(result).print(STDOUT, 0)
+def with_substr(s)
+  a = [s.to_sym]
+  i = 0
+  loop do
+    break unless i = s.index('.',i)
+    i += 1
+    a << s[i..-1].to_sym
+  end
+  a
+end
 
+test_string = 'messages.index.filter.label'
+test_methods = [:with_inject, :with_block, :with_substr]
 count = 100000
 
-Benchmark.bm do |b|
-  b.report('inject') do
-    count.times do
-      segments = "messages.index.filter.label".split('.').reverse
-      segments.inject([segments.shift.to_sym]){|a,s| a.unshift "#{s}.#{a.first}".to_sym}
-    end
+test_methods.each do |method|
+  result = RubyProf.profile do
+    send(method, test_string)
   end
+  RubyProf::GraphPrinter.new(result).print(STDOUT, 0)
+end
 
-  b.report('block') do
-    count.times do
-      a = "messages.index.filter.label".split('.')
-      1.upto(a.size).inject([]) {|s,t| s << a.slice(a.size-t,t).join('.').to_sym}.reverse
+Benchmark.bm do |b|
+  test_methods.each do |method|
+    b.report(method.to_s) do
+      count.times do
+        send(method, test_string)
+      end
     end
   end
 end
